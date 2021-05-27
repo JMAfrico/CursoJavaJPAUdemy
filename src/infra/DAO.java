d2f827f9f8922befb069ef3243977a34f4802cd0
JMAfrico <joao_marcossilva@hotmail.com>
2021-05-27 20:30:00 -0300

    Classe Data Acess Object (DAO) Criada

---
+++ src/infra/DAO.java
@@ -0,0 +1,95 @@
+package infra;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.swing.JOptionPane;
+
+//classe genérica,nome para chamar ela mesma
+public class DAO <Tipo>{
+
+	private static EntityManagerFactory fabrica;
+	private static EntityManager em;
+	private Class<Tipo> classe;
+	
+	//BLOCO STATIC INICIALIZA ASSIM QUE INICIA O SISTEMA
+	static {
+		try {
+			
+			fabrica = Persistence.createEntityManagerFactory("Exercicios_JPA");
+		
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro: "+e);
+		}
+	}
+	
+	public DAO() {
+		this(null);
+	}
+	
+	
+	public DAO(Class<Tipo> classe) {
+		this.classe = classe;
+		em = fabrica.createEntityManager();
+	}
+	
+	public DAO<Tipo> abrirTransacao() {
+		em.getTransaction().begin();
+		return this;
+	}
+	
+	public DAO<Tipo> fecharTransacao() {
+		em.getTransaction().commit();
+		return this;
+	}
+	
+	public DAO<Tipo> buscarID(Long id) {
+		em.find(classe, id);
+		return this;
+	}
+	
+	public DAO<Tipo> autenticacao(Tipo classe) {
+		em.detach(classe);
+		em.merge(classe);
+		return this;
+	}
+	
+	public DAO<Tipo> incluirTransacao(Tipo entidade) {
+		em.persist(entidade);
+		return this;
+	}
+	
+	public DAO<Tipo> incluirCompleto(Tipo entidade) {
+		return this.abrirTransacao().incluirTransacao(entidade).fecharTransacao();
+	}
+	
+	public List<Tipo> obterTodos (int quantidade, int deslocamento) {
+		if(classe == null) {
+			throw new UnsupportedOperationException("Classe nula.");
+		}
+		
+		String jpql = "select e from "+ classe.getName() +" e";
+		TypedQuery<Tipo> query = em.createQuery(jpql, classe);
+		query.setMaxResults(quantidade);
+		query.setFirstResult(deslocamento);
+		return query.getResultList();
+		
+	}
+	
+	public DAO<Tipo> alterarCompleto(Long id) {
+		return this.abrirTransacao().buscarID(id).autenticacao().fecharTransacao();
+	}
+	
+	public DAO<Tipo> deletarCompleto() {
+		em.getTransaction().commit();
+		return this;
+	}
+	
+	public void fechar() {
+		em.close();
+	}
+	
+	
+}
---
+++ src/teste/basico/NovoProduto.java
@@ -0,0 +1,33 @@
+package teste.basico;
+
+import java.util.List;
+
+import infra.DAO;
+import modelo.basico.Produto;
+
+public class NovoProduto {
+
+
+	public static void main(String[] args) {
+		
+		//Cria um DAO do tipo Produto(Já que a classe é genérica!)
+		DAO<Produto> dao = new DAO<>(Produto.class);
+
+		//NOVO PRODUTO
+		Produto produto = new Produto("Bicicleta Caloi", 150.15);		
+		//dao.incluirCompleto(produto).fechar();
+		
+		//SELECT PRODUTOS
+		//List<Produto> produtos = dao.obterTodos(10, 0);
+		//
+		//for(Produto p: produtos ) {
+		//	System.out.println(p.getNome());
+		//}
+		
+		//ALTERAR PRODUTOS
+		
+		//DELETAR PRODUTOS
+		
+		
+	}
+}
